b97de8bf64a75d1a7fc2577ed57b28027586990b
Gaurav SJB Rana <bitcoinssg@gmail.com>
2017-11-04 21:36:39 -0400

    asdfa

---
+++ test-truf-zep/contracts/agc-acx.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.4;
+
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
+contract AccessCoin is StandardToken {
+
+string public name = 'AccessCoin';
+string public symbol = 'ACX';
+uint public decimals = 10;
+uint public INITIAL_SUPPLY = 6000000000;
+
+}
+
+
+
+
